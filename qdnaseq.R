c13cdc06527ca03b5574d65b9bcb49fea21e918d
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-05-24 07:46:35 +0200

    make background optional. Without supplying a background, calling of amplifications and deletions is not run since chrcall crashes for single samples. However, segmentation is performed and written to an output file.

--- qdnaseq.R
+++ qdnaseq.R
@@ -8,7 +8,7 @@ library(getopt)
 #argmask 0=no arg, 1=req, 2=optional
 args <- rbind(c("bam", "b", 1, "character", "Tumor low-pass WGS bam file"),
 	      c("output", "o", 1, "character", "Output tsv with bed file with segmented copy numbers"),
-	      c("background", "x", 1, "character", "Background set to use, as a RData file"))
+	      c("background", "x", 2, "character", "Background set to use, as a RData file"))
 
 # opts <- getopt(args,opt = c("--bam", "~/L11466T_wgs.bam",
 #                            "--output", "~/tmp/L11466T_wgs.qdnaseq.txt",
@@ -21,9 +21,9 @@ if(is.null(opts$bam)){
 if(is.null(opts$output)){
   stop("Must specify output tsv file name --segments/-s.")
 }
-if(is.null(opts$background)){
-  stop("Must specify background file --background/-x.")
-}
+# if(is.null(opts$background)){
+#   stop("Must specify background file --background/-x.")
+# }
 
 ###########################################
 # load packages
@@ -34,16 +34,23 @@ library(Biobase)
 
 ###########################################
 # run qdnaseq
+if(! is.null(opts$background)){
+  bgReadCounts <- readRDS(opts$background)
 
-bgReadCounts <- readRDS(opts$background)
+}
 
 bins <- getBinAnnotations(binSize=15)
 readCounts <- binReadCounts(bins, bamfiles=opts$bam)
 sampleName <- readCounts@phenoData@data$name
-if( sampleName %in% bgReadCounts@phenoData@data$name ){
-  allReadCounts <- bgReadCounts
-} else {
-  allReadCounts <- combine(readCounts, bgReadCounts)
+
+if(! is.null(opts$background)){
+  if( sampleName %in% bgReadCounts@phenoData@data$name ){
+    allReadCounts <- bgReadCounts
+  } else {
+    allReadCounts <- combine(readCounts, bgReadCounts)
+  }
+}else{
+  allReadCounts <- readCounts 
 }
 
 readCountsFiltered <- applyFilters(allReadCounts,residual=TRUE, blacklist=TRUE)
@@ -53,24 +60,26 @@ copyNumbersNormalized <- normalizeBins(copyNumbers)
 copyNumbersSmooth <- smoothOutlierBins(copyNumbersNormalized)
 copyNumbersSegmented <- segmentBins(copyNumbersSmooth)
 copyNumbersSegmentedNormalized <- normalizeSegmentedBins(copyNumbersSegmented)
-copyNumbersCalled <- callBins(copyNumbersSegmentedNormalized, cellularity=.75)
-
+if(! is.null(opts$background) ){
+  copyNumbersCalled <- callBins(copyNumbersSegmentedNormalized, cellularity=.75)
+}
 ##################################################################
 ## Make final data matrix
 
-dat <- data.table( copyNumbersCalled@featureData@data )
+dat <- data.table( copyNumbersSegmentedNormalized@featureData@data )
 dat[, readcount  := readCounts@assayData$counts]
-dat[, copynumber := copynumber(copyNumbersCalled)[,sampleName] ]
-dat[, segmented  := segmented(copyNumbersCalled)[,sampleName] ]
-dat[, call       := calls(copyNumbersCalled)[,sampleName] ]
-dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
-dat[, probloss   := probloss(copyNumbersCalled)[,sampleName] ]
-dat[, probnorm   := probnorm(copyNumbersCalled)[,sampleName] ]
-dat[, probgain   := probgain(copyNumbersCalled)[,sampleName] ]
-dat[, probamp    := probamp(copyNumbersCalled)[,sampleName] ]
-dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
-dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
-
+dat[, copynumber := copynumber(copyNumbersSegmentedNormalized)[,sampleName] ]
+dat[, segmented  := segmented(copyNumbersSegmentedNormalized)[,sampleName] ]
+if(! is.null(opts$background) ){
+  dat[, call       := calls(copyNumbersCalled)[,sampleName] ]
+  dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
+  dat[, probloss   := probloss(copyNumbersCalled)[,sampleName] ]
+  dat[, probnorm   := probnorm(copyNumbersCalled)[,sampleName] ]
+  dat[, probgain   := probgain(copyNumbersCalled)[,sampleName] ]
+  dat[, probamp    := probamp(copyNumbersCalled)[,sampleName] ]
+  dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
+  dat[, probdloss  := probdloss(copyNumbersCalled)[,sampleName] ]
+}
 ##################################################################
 ## Supress scientific notation in output files
 options(scipen=999)
