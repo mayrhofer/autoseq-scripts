5e373e9b83dd10a54be9352b8c67f4b676039177
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-09-30 09:44:50 +0200

    build and upload a conda package on travis

---
+++ recipe/build.sh
@@ -0,0 +1,23 @@
+FILES=( GenomeAnalysisTK-3.5.jar \ \
+                GenomeAnalysisTK-Klevebring.jar \
+        alasccaCNA.R \
+        bed_to_regions.py \
+        cnvgtf2bed.py \
+        filter_erroneus_alt.py \
+        gatk-klevebring \
+        gtf_filter_unused_chrs.py \
+        intersect-msi-sites.sh \
+        msisensor \
+        mutect2 \
+        picard_interval_list_to_bed6_converter.py \
+        qdnaseq.R \
+        qdnaseq2bed.py \
+        slopIntervalList.py \
+        target_coverage_histogram.py \
+        vcf_add_sample.py \
+        vcfsorter.pl \
+        )
+
+for F in ${FILES[@]}; do
+  cp $F $PREFIX/bin/$F
+done
